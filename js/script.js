42db706f8322a32a8c11dba1cece1ecbeb93fbc3
Gianluca <gl.galassini@gmail.com>
2021-06-22 17:52:55 +0200

    creo app

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const app = new Vue (
+    {
+        el:"#app",
+        data: {
+            pics: [
+                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=1",
+        ]
+        }
+    }
+);
