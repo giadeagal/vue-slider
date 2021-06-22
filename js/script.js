5b5b568316113186ef58c4fd470c1c6ae175d07b
Gianluca <gl.galassini@gmail.com>
2021-06-22 18:00:04 +0200

    cambio random seed alle immagini

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const app = new Vue (
         data: {
             pics: [
                 "https://picsum.photos/800/500?random=1",
-                "https://picsum.photos/800/500?random=1",
-                "https://picsum.photos/800/500?random=1",
-                "https://picsum.photos/800/500?random=1",
-                "https://picsum.photos/800/500?random=1",
-                "https://picsum.photos/800/500?random=1",
+                "https://picsum.photos/800/500?random=2",
+                "https://picsum.photos/800/500?random=3",
+                "https://picsum.photos/800/500?random=4",
+                "https://picsum.photos/800/500?random=5",
+                "https://picsum.photos/800/500?random=6",
         ]
         }
     }
